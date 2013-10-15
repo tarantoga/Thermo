a5f25f67165dd0cd879b6762ecfefaf0a29a2e29
Artur Konarski <tarantoga@me.com>
2013-10-15 23:47:39 +0200

    graph added

--- api/controllers/SampleController.js
+++ api/controllers/SampleController.js
@@ -8,6 +8,7 @@
 module.exports = {
   
 	last: function (req, res) {
+
 		Sample.find({ device: 'AA' }).limit(1).sort('createdAt DESC').done(function(err, samples) {
 			if (err) {
 				return console.log(err);
@@ -16,10 +17,7 @@ module.exports = {
 					res.view({ temp: '--.--' });
 				}else{
 					var gmt = samples[0].createdAt;
-					//var local = new Date(gmt.getTime() - (gmt.getTimezoneOffset() * 60000));
-					//console.log(localDate.toString());
 					var formatted = DateHelper.formatLocal(gmt, "dd.MM.yyyy hh:mm:ss");
-					console.log(formatted);
 					res.view({ temp: samples[0].value.toFixed(1), time: formatted });
 				}
 			}
@@ -28,7 +26,21 @@ module.exports = {
 
 	subscribe: function (req, res) {
 		Sample.subscribe(req.socket);
-		res.send();
+		//send initial graph data - last 300 samples = latest 150 minutes
+		Sample.find().limit(300).sort('createdAt DESC').done(function(err, samples) {
+
+			// Error handling
+			if (err) {
+				return console.log(err);
+			} else {
+				var vals = new Array();
+				for (var i = samples.length - 1; i >= 0 ; i--) {
+					vals.push(samples[i].value);
+				}
+				req.socket.emit('graphdata', {values: vals});
+				res.send();
+			}
+		});
 	}
  
 };
